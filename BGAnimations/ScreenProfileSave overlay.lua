c9445780c86457c7fa9fcdc1b0213c4fe28069cd
unknown <madcat182@gmail.com>
2015-09-11 23:44:42 -0500

    Removed saving profiles message

---
+++ BGAnimations/ScreenProfileSave overlay.lua
@@ -0,0 +1,12 @@
+local x = Def.ActorFrame{
+};
+
+x[#x+1] = Def.Actor {
+	BeginCommand=function(self)
+		if SCREENMAN:GetTopScreen():HaveProfileToSave() then self:sleep(0.01); end;
+		self:queuecommand("Load");
+	end;
+	LoadCommand=function() SCREENMAN:GetTopScreen():Continue(); end;
+};
+
+return x;
