214e7111008f648edc7975bc39045bc177881728
unknown <madcat182@gmail.com>
2017-04-02 10:40:12 -0500

    Added screen stage information

---
+++ BGAnimations/ScreenStageInformation decorations/default.lua
@@ -0,0 +1,51 @@
+local t = Def.ActorFrame {};
+local sTime = .1;
+
+t[#t+1] = Def.ActorFrame {
+	Def.Quad{
+		OnCommand=cmd(FullScreen;diffusecolor,color("#000000"));
+	};
+};
+
+t[#t+1] = Def.ActorFrame {
+	OnCommand=cmd(Center);
+	LoadActor("00")..{
+		OnCommand=cmd(x,-44);
+	};
+	LoadActor("00")..{
+		OnCommand=cmd(x,44);
+	};
+	LoadActor("00")..{
+		OnCommand=cmd(x,-132);
+	};
+	LoadActor("00")..{
+		OnCommand=cmd(x,132);
+	};
+	LoadActor("00")..{
+		OnCommand=cmd(x,-220);
+	};
+	LoadActor("00")..{
+		OnCommand=cmd(x,220);
+	};
+};
+
+t[#t+1] = Def.ActorFrame {
+	OnCommand=cmd(Center);
+	LoadActor("01")..{
+		OnCommand=cmd(playcommand,"Anim");
+		AnimCommand=cmd(diffusealpha,0;sleep,sTime;diffusealpha,1;x,-220;sleep,sTime;x,-132;sleep,sTime;x,-44;sleep,sTime;x,44;sleep,sTime;x,132;sleep,sTime;x,220;queuecommand,"repeat");
+		repeatCommand=cmd(sleep,sTime;queuecommand,"Anim");
+	};
+	LoadActor("02")..{
+		OnCommand=cmd(diffusealpha,0;sleep,sTime;playcommand,"Anim");
+		AnimCommand=cmd(diffusealpha,0;sleep,sTime;diffusealpha,1;x,-220;sleep,sTime;x,-132;sleep,sTime;x,-44;sleep,sTime;x,44;sleep,sTime;x,132;sleep,sTime;x,220;queuecommand,"repeat");
+		repeatCommand=cmd(sleep,sTime;queuecommand,"Anim");
+	};
+	LoadActor("01")..{
+		OnCommand=cmd(diffusealpha,0;sleep,sTime*2;playcommand,"Anim");
+		AnimCommand=cmd(diffusealpha,0;sleep,sTime;diffusealpha,1;x,-220;sleep,sTime;x,-132;sleep,sTime;x,-44;sleep,sTime;x,44;sleep,sTime;x,132;sleep,sTime;x,220;queuecommand,"repeat");
+		repeatCommand=cmd(sleep,sTime;queuecommand,"Anim");
+	};
+};
+
+return t;
