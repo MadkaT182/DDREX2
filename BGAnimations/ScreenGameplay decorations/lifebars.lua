738ec3f21df14471d8400f4f96e71fca612b2b23
unknown <madcat182@gmail.com>
2021-10-18 17:14:39 -0500

    Lifebar update

--- BGAnimations/ScreenGameplay decorations/lifebars.lua
+++ BGAnimations/ScreenGameplay decorations/lifebars.lua
@@ -10,48 +10,68 @@ for pn in ivalues(GAMESTATE:GetEnabledPlayers()) do
 		Def.ActorFrame{
 			InitCommand=function(self) if pn == PLAYER_1 then self:diffusealpha(1):x(SCREEN_CENTER_X-281) else self:diffusealpha(0) end end;
 			--Life Fill
-			LoadActor("_life/normal")..{
+			Def.Quad{
+				InitCommand=cmd(x,4;diffuse,color("#3C3C3CBB");zoomto,barw,barh;skewx,-.1);
+			};
+			Def.Quad{
+				InitCommand=cmd(x,4;blend,Blend.Add;zoomto,barw,barh;skewx,-.1;diffuseshift;effectcolor1,1,0,0,.4;effectcolor2,1,0,0,0;effectperiod,1.5;diffusealpha,0);
+				HealthStateChangedMessageCommand=function(self, param)
+					if param.PlayerNumber == PLAYER_1 then
+						if param.HealthState == "HealthState_Danger" then
+							self:diffusealpha(1);
+						else
+							self:diffusealpha(0);
+						end;
+					end;
+				end;
+			};
+			LoadActor(THEME:GetPathG("StreamDisplay","normal"))..{
 				InitCommand=function(self)
 					self:x(3);
 					self:texcoordvelocity(.8,0);
 					self:skewx(-4.5);
 					self:zoomto(barw,barh);
 					self:cropright(1);
-					self:linear(1);
+					self:sleep(.5);
+					self:linear(.9);
 					self:cropright(.5);
-				end;
-				LifeChangedMessageCommand=function(self, param)
-					if param.Player == PLAYER_1 then
-						self:stoptweening();
-						self:cropright(1-param.LifeMeter:GetLife());
-						self:linear(.25);
-						self:cropright(1-param.LifeMeter:GetLife()+0.025);
-						self:linear(.25);
-						self:cropright(1-param.LifeMeter:GetLife()-0.05);
-						self:linear(.25);
-						self:cropright(1-param.LifeMeter:GetLife()+0.025);
-						self:linear(.25);
-						self:cropright(1-param.LifeMeter:GetLife());
-					end;
-				end;
-			};
-			LoadActor("_life/danger")..{
-				InitCommand=function(self)
-					self:x(3);
-					self:texcoordvelocity(1.5,0);
-					self:skewx(-1);
-					self:zoomto(barw,barh);
-					self:cropright(1);
 					self:linear(.5);
-					self:cropright(.75);
-					self:sleep(0);
-					self:linear(.25);
-					self:diffusealpha(0);
+					self:cropright(.5);
+					self:linear(.5);
+					self:cropright(.55);
+					self:linear(.5);
+					self:cropright(.5);
 				end;
 				HealthStateChangedMessageCommand=function(self, param)
 					if param.PlayerNumber == PLAYER_1 then
 						if param.HealthState == "HealthState_Danger" then
-							self:linear(1);
+							self:diffusealpha(0);
+						else
+							self:diffusealpha(1);
+						end;
+					end;
+				end;
+				LifeChangedMessageCommand=function(self, param)
+					if param.Player == PLAYER_1 then
+						self:stoptweening();
+						self:linear(.3);
+						self:cropright(1-param.LifeMeter:GetLife()+.025);
+						self:linear(.1);
+						self:cropright(1-param.LifeMeter:GetLife());
+					end;
+				end;
+			};
+			LoadActor(THEME:GetPathG("StreamDisplay","danger"))..{
+				InitCommand=function(self)
+					self:diffusealpha(0);
+					self:x(3);
+					self:texcoordvelocity(1.5,0);
+					self:skewx(-1);
+					self:zoomto(barw,barh);
+				end;
+				HealthStateChangedMessageCommand=function(self, param)
+					if param.PlayerNumber == PLAYER_1 then
+						if param.HealthState == "HealthState_Danger" then
 							self:diffusealpha(1);
 						else
 							self:diffusealpha(0);
@@ -60,33 +80,29 @@ for pn in ivalues(GAMESTATE:GetEnabledPlayers()) do
 				end;
 				LifeChangedMessageCommand=function(self, param)
 					if param.Player == PLAYER_1 then
-						if param.HealthState == "HealthState_Danger" then
-							self:stoptweening();
-							self:cropright(1-param.LifeMeter:GetLife());
-							self:linear(.25);
-							self:cropright(1-param.LifeMeter:GetLife()+0.025);
-							self:linear(.25);
-							self:cropright(1-param.LifeMeter:GetLife()-0.05);
-							self:linear(.25);
-							self:cropright(1-param.LifeMeter:GetLife()+0.025);
-							self:linear(.25);
-							self:cropright(1-param.LifeMeter:GetLife());
-						end;
+						self:stoptweening();
+						self:linear(.3);
+						self:cropright(1-param.LifeMeter:GetLife()+.025);
+						self:linear(.1);
+						self:cropright(1-param.LifeMeter:GetLife());
 					end;
 				end;
 			};
-			LoadActor("_life/full")..{
+			LoadActor(THEME:GetPathG("StreamDisplay","hot"))..{
 				InitCommand=function(self)
 					self:x(3);
 					self:texcoordvelocity(.8,0)
 					self:skewx(-4.5);
 					self:zoomto(barw+4,barh);
+					self:glowblink();
+					self:effectcolor1(1,1,1,0);
+					self:effectcolor2(1,1,1,.5);
+					self:effectperiod(.075);
 					self:diffusealpha(0);
 				end;
 				HealthStateChangedMessageCommand=function(self, param)
 					if param.PlayerNumber == PLAYER_1 then
 						if param.HealthState == "HealthState_Hot" then
-							self:linear(1);
 							self:diffusealpha(1);
 						else
 							self:diffusealpha(0);
@@ -97,9 +113,9 @@ for pn in ivalues(GAMESTATE:GetEnabledPlayers()) do
 			LoadActor("_life/grid")..{
 				InitCommand=cmd(x,3);
 			};
-			LoadActor("_life/frame")..{};
+			LoadActor("_life/frame");
 			LoadActor("_life/dng_lbl")..{
-				OnCommand=cmd(diffusealpha,0;x,42;diffuseshift;effectcolor1,color("#FFFF00");effectcolor2,color("#FFFF0000"));
+				OnCommand=cmd(diffusealpha,0;x,42;diffuseshift;effectcolor1,color("#FFFF00");effectcolor2,color("#FFFF0000");effectperiod,.8);
 				HealthStateChangedMessageCommand=function(self, param)
 					if param.PlayerNumber == PLAYER_1 then
 						if param.HealthState == "HealthState_Danger" then
@@ -109,6 +125,10 @@ for pn in ivalues(GAMESTATE:GetEnabledPlayers()) do
 						end;
 					end;
 				end;
+			};
+			LoadActor("_life/highlight")..{
+				InitCommand=cmd(y,3;blend,Blend.Add;cropright,1;diffusealpha,.5;queuecommand,"Anim";player,PLAYER_1);
+				AnimCommand=cmd(diffusealpha,.5;cropright,1;sleep,.2;linear,200/(GAMESTATE:GetSongBPS()*100);cropright,0;diffusealpha,0;sleep,.2;queuecommand,"Anim");
 			};
 			--Player Indicator
 			Def.ActorFrame{
@@ -123,7 +143,7 @@ for pn in ivalues(GAMESTATE:GetEnabledPlayers()) do
 		Def.ActorFrame{
 			InitCommand=function(self) if pn == PLAYER_2 then self:diffusealpha(1):x(SCREEN_CENTER_X+281) else self:diffusealpha(0) end end;
 			--Life Fill
-			LoadActor("_life/normal")..{
+			LoadActor(THEME:GetPathG("StreamDisplay","normal"))..{
 				InitCommand=function(self)
 					self:x(-3);
 					self:texcoordvelocity(-.8,0)
@@ -139,7 +159,7 @@ for pn in ivalues(GAMESTATE:GetEnabledPlayers()) do
 					end;
 				end;
 			};
-			LoadActor("_life/danger")..{
+			LoadActor(THEME:GetPathG("StreamDisplay","danger"))..{
 				InitCommand=function(self)
 					self:x(-3);
 					self:texcoordvelocity(-1.5,0);
@@ -163,7 +183,7 @@ for pn in ivalues(GAMESTATE:GetEnabledPlayers()) do
 					end;
 				end;
 			};
-			LoadActor("_life/full")..{
+			LoadActor(THEME:GetPathG("StreamDisplay","hot"))..{
 				InitCommand=function(self)
 					self:x(-3);
 					self:texcoordvelocity(-.8,0)
